756d8d78b938bd39cf0fe1885cf0572cf233f8a2
erinalara <48671321+erinalara@users.noreply.github.com>
2020-04-09 21:13:14 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include "ModernLinkedList.h"
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	ModernLinkedList ps;
+
+	ps.PushFront(1);
+	ps.PushFront(2);
+	ps.PushFront(3);
+	ps.PushFront(4);
+	ps.PushFront(5);
+/*
+	ps.PrintList();
+	ps.PushBack(8);
+
+	ps.PrintList();
+
+
+	cout << "Popped back: ";
+	ps.PopBack();
+	ps.PrintList();
+
+	cout << "Popped front: ";
+	ps.PopFront();
+	ps.PrintList();
+
+	cout << endl;
+
+	//ps.Insert(3,7);
+	ps.PrintList();
+
+	ps.RemoveAt(3);
+	ps.PrintList();
+
+	ps.Clear();
+	ps.PrintList();
+
+	cout << "position " << ps[2] << endl;
+	ps.PrintList();
+
+
+	return 0;*/
+}
